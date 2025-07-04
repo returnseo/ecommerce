bd392336bece58cabffe2f7b67620f5bf04f8e87
returnseo <reseo4150@gmail.com>
2025-07-04 18:28:19 +0900

    1주차 과제

--- src/main/java/com/study/ecommerce/domain/payment/processor/impl/BankTransferProcessor.java
+++ src/main/java/com/study/ecommerce/domain/payment/processor/impl/BankTransferProcessor.java
@@ -6,6 +6,8 @@ import com.study.ecommerce.domain.payment.processor.PaymentProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Slf4j
 @Component
 public class BankTransferProcessor implements PaymentProcessor {
@@ -14,23 +16,54 @@ public class BankTransferProcessor implements PaymentProcessor {
 
     @Override
     public PaymentResult process(PaymentRequest request) {
-        return null;
+        log.info("계좌번호로 송금 시작 - 주문ID: {}, 금액: {}", request.orderId(), request.amount());
+        // 계좌 유효성 검증
+        if(!isValidAccount(request.accountNumber())) {
+            return PaymentResult.builder()
+                    .success(false)
+                    .message("유효하지 않은 계좌 입니다.")
+                    .paymentMethod("ACCOUNT")
+                    .build();
+        }
+        // 송금 한도 확인
+        if(request.amount() > MAX_AMOUNT) {
+            return PaymentResult.builder()
+                    .success(false)
+                    .message("송금 한도를 초과했습니다.")
+                    .paymentMethod("ACCOUNT_TRANSFER")
+                    .build();
+        }
+
+        String transactionId = UUID.randomUUID().toString();
+        int feeAmount = calculateFee(request.amount());
+
+        return PaymentResult.builder()
+                .success(true)
+                .transactionId(transactionId)
+                .message("송금 결제가 완료되었습니다.")
+                .paidAmount(request.amount())
+                .feeAmount(feeAmount)
+                .paymentMethod("ACCOUNT_TRANSFER")
+                .build();
     }
 
     @Override
     public int calculateFee(int amount) {
-        return 0;
+        return FIXED_FEE;
     }
 
     @Override
     public boolean supports(String paymentMethod) {
-        return false;
+        return "ACCOUNT_TRANSFER".equals(paymentMethod);
     }
 
     @Override
     public int getMaxAmount() {
-        return 0;
+        return MAX_AMOUNT;
     }
 
-    // isValidAccount -> 10자리이상, 14자리 이하
+    // isValidAccount -> 10자리 이상 14자리 이하
+    public boolean isValidAccount(String accountNumber) {
+        return accountNumber != null && accountNumber.length() >= 10 && accountNumber.length() <= 14;
+    }
 }
--- src/main/java/com/study/ecommerce/domain/payment/processor/impl/SimplePayProcessor.java
+++ src/main/java/com/study/ecommerce/domain/payment/processor/impl/SimplePayProcessor.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -20,23 +21,55 @@ public class SimplePayProcessor implements PaymentProcessor {
 
     @Override
     public PaymentResult process(PaymentRequest request) {
-        return null;
+
+        // 결제 유효성 검증
+        if (!isSupportedProvider(request.cardNumber())) {
+            return PaymentResult.builder()
+                    .success(false)
+                    .message("사용 불가능한 간편 결제 입니다.")
+                    .paymentMethod("SIMPLE_PAYMENT")
+                    .build();
+        }
+
+        // 결제 한도를 확인
+        if (request.amount() > MAX_AMOUNT) {
+            return PaymentResult.builder()
+                    .success(false)
+                    .message("결제 한도를 초과했습니다.")
+                    .paymentMethod("SIMPLE_PAYMENT")
+                    .build();
+        }
+
+        String transactionId = UUID.randomUUID().toString();
+        int feeAmount = calculateFee(request.amount());
+
+        return PaymentResult.builder()
+                .success(true)
+                .transactionId(transactionId)
+                .message("간편 결제가 완료되었습니다.")
+                .paidAmount(request.amount())
+                .feeAmount(feeAmount)
+                .paymentMethod("SIMPLE_PAYMENT")
+                .build();
     }
 
     @Override
     public int calculateFee(int amount) {
-        return 0;
+        return (int) (amount * FEE_RATE);
     }
 
     @Override
     public boolean supports(String paymentMethod) {
-        return false;
+        return "SIMPLE_PAYMENT".equals(paymentMethod);
     }
 
     @Override
     public int getMaxAmount() {
-        return 0;
+        return MAX_AMOUNT;
     }
 
     // isSupportedProvider(String provider)
+    private boolean isSupportedProvider(String payment) {
+        return payment != null && SUPPORTED_PROVIDERS.contains(payment);
+    }
 }
